4cb35a1a20e3e47df53f9cb9ea1014cccba4c618
Seongjoo Jin <evanjin8@gmail.com>
2019-11-26 13:28:07 +0900

    remove store folder

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,27 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import configureStore from './store'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const configureStore = () => {
+  const store =
+    process.env.NODE_ENV === 'development'
+      ? createStore(persistedReducer, composeWithDevTools())
+      : createStore(persistedReducer)
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
 
 const { store, persistor } = configureStore()
 
